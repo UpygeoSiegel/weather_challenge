2911cfd5a9491b9f00ee3b2cc6d17cb543d4a824
Benjamin Siegel <bsiegel@upchs.org>
2018-03-20 21:17:15 +0000

    commit message

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function searchWeather(){
-    var API_KEY = ''
-    var URL = ''
+    var API_KEY = 'a4f3595d529f46e4bc9719b17e1b7938'
+    var URL = 'https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=' + API_KEY
     
-    fetch()
-    .then(function(){
+    fetch(URL)
+    .then(function(data){
+        return data.json()
     })
-    .then(function(){
+    .then(function(weather){
+        console.log(weather)    
     })
     .catch(function() {
     });
